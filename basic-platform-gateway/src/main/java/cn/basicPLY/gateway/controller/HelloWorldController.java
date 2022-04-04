e4423a5d1749ea3ec4ea1a23367601cb4cb1461b
Pan Liuyang <1679814115@qq.com>
2022-04-04 20:18:22 +0800

    初始项目提交项

---
+++ basic-platform-gateway/src/main/java/cn/basicPLY/gateway/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package cn.basicPLY.gateway.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * purpose:
+ *
+ * @author Pan Liuyang
+ * 2022/4/4 17:57
+ */
+@RestController
+public class HelloWorldController {
+}
